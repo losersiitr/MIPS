4c47f40715e27fb08655b01c26948510ac13ffab
losersiitr <32241097+losersiitr@users.noreply.github.com>
2017-09-27 21:40:32 +0530

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -31,67 +31,37 @@ module data_memory
 	wren
 );
 
-    //--------------------------
-	// Parameters
-	//--------------------------	
+    //Following are the input ports to one of the modules.
+    
+	input clk;
+        input [31:0] addr;
+	input [31:0] wdata;
+	input [3:0] wren;
 	
-    //--------------------------
-	// Input Ports
-	//--------------------------
-	// < Enter Input Ports  >
-	input				clk;
-    input 		[31:0]	addr;
-	input		[31:0]	wdata;
-	input 		[3:0]	wren;
-	
-    //--------------------------
-    // Output Ports
-    //--------------------------
-    // < Enter Output Ports  >	
-    output 		[31:0] 	rdata;
-		
-    //--------------------------
-    // Bidirectional Ports
-    //--------------------------
-    // < Enter Bidirectional Ports in Alphabetical Order >
-    // None
-      
-    ///////////////////////////////////////////////////////////////////
-    // Begin Design
-    ///////////////////////////////////////////////////////////////////
-    //-------------------------------------------------
-    // Signal Declarations: local params
-    //-------------------------------------------------
+   //The only output port which reads data, is assigned a variable rdata.
    
-    //-------------------------------------------------
-    // Signal Declarations: reg
-    //-------------------------------------------------    
-    reg	[7:0] memory_lane0	[65535:0];
-	reg	[7:0] memory_lane1	[65535:0];
-	reg	[7:0] memory_lane2	[65535:0];
-	reg	[7:0] memory_lane3	[65535:0];
-    //-------------------------------------------------
-    // Signal Declarations: wire
-    //-------------------------------------------------
-		
-	//---------------------------------------------------------------
-	// Instantiations
-	//---------------------------------------------------------------
-	// None
+    output [31:0] rdata;
 
-	//---------------------------------------------------------------
-	// Combinatorial Logic
-	//---------------------------------------------------------------
+    //We have here 4 memory units, each of which has 65536 (8 bit locations). 
+ 
+    reg	[7:0] memory_lane0	[65535:0];
+    reg	[7:0] memory_lane1	[65535:0];
+    reg	[7:0] memory_lane2	[65535:0];
+    reg	[7:0] memory_lane3	[65535:0];
+    
+    //The output port takes up 8 bit from each of the memory unit, adding up to 32 bit data which was supposed to be read.
+    //This is done using the command assign.
+   
 	assign rdata = {
 						memory_lane3[addr[17:2]],
 						memory_lane2[addr[17:2]],
 						memory_lane1[addr[17:2]],
 						memory_lane0[addr[17:2]]
 					};
-							
-	//---------------------------------------------------------------
-	// Sequential Logic
-	//---------------------------------------------------------------
+    //Our operations are active on the rising edge i.e. the positive clock edge.
+    //If write enable(denoted by wren[i]) is assigned a value 1, the data is assigned to a particular location in the memory unit.
+    						
+	
     always @(posedge clk)
 	begin
 		if (wren[0])
